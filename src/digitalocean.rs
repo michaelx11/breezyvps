71141aad99210c97c2062506c4212928298d7c20
Michael Xu <michaeljxu11@gmail.com>
2017-08-09 21:51:28 -0700

    port create_droplet to command chain

--- src/digitalocean.rs
+++ src/digitalocean.rs
@@ -1,58 +1,64 @@
 use super::command;
-
-fn get_all_sshkey_ids() -> String {
-    let get_sshkeys_cmd = "doctl compute ssh-key list --no-header --format=ID";
-    println!("Running command:\n\t\t{}", get_sshkeys_cmd);
-    let result = command::run_host_cmd(&get_sshkeys_cmd);
-    if !result.success {
-        println!("Failed to get sshkeys with stderr:\n\n{}", result.stderr);
-    }
-    let ids : Vec<&str> = result.stdout.lines().collect();
-    return ids.join(",");
-}
+use super::chain;
 
 pub fn create_droplet_by_name(name: &str) {
-    let create_str = format!("doctl compute droplet create {} --image=ubuntu-16-04-x64 --region=sfo1 --size=512mb --ssh-keys=\"{}\" --wait", name, get_all_sshkey_ids());
-    println!("Running command:\n\t\t{}", create_str);
-    // Create the actual droplet
-    let result = command::run_host_cmd(&create_str);
-    if !result.success {
-        println!("Failed, with stderr:\n\n{}", result.stderr);
-        return;
-    }
 
-    let list_command = "doctl compute droplet list --format Name,PublicIPv4,PublicIPv6,Status";
-    println!("Running command:\n\t\t{}", list_command);
-    let droplet_result = command::run_host_cmd(&list_command);
-    if !droplet_result.success{
-        println!("Failed, with stderr:\n\n{}", droplet_result.stderr);
-        return;
-    }
-    let lines = droplet_result.stdout.lines();
-    let mut ip_address = "";
-    for line in lines {
-        if line.starts_with(name) {
-            println!("Found: {}", line);
-            let fields : Vec<&str> = line.split_whitespace().collect();
-            if fields.len() < 2 {
-                println!("Couldn't find ip address in line: {}", line);
-                return;
+    let ssh_key_mapping_func = |res: &command::Result, cmd_str: String| -> String {
+        let ids : Vec<&str> = res.stdout.lines().collect();
+        let new_cmd = str::replace(&cmd_str, "%ssh_keys%", &ids.join(","));
+        new_cmd.to_string()
+    };
+
+    let ip_address_mapping_func = |res: &command::Result, cmd_str: String| -> String {
+        let new_cmd = str::replace(&cmd_str, "%ip_address%", &res.stdout);
+        new_cmd.to_string()
+    };
+
+    // Get ip address from stdout
+    let ip_address_extractor = |res: &command::Result| -> command::Result {
+        let mut ip_address : Option<String> = None;
+        let res_stdout = res.stdout.clone();
+        let lines = res_stdout.lines();
+        for line in lines {
+            if line.starts_with(name) {
+                debug!("Found: {}", line);
+                let fields : Vec<&str> = line.split_whitespace().collect();
+                if fields.len() < 2 {
+                    warn!("Couldn't find ip address in line: {}", line);
+                }
+                ip_address = Some(fields[1].to_string());
+                break;
             }
-            ip_address = fields[1];
-            break;
         }
-    }
-    if ip_address.len() == 0 {
-        println!("Couldn't locate droplet in output:\n\n{}", droplet_result.stdout);
-        return;
-    }
-    // Create a DNS record to point to the droplet
-    let record_str = format!("doctl compute domain records create one.haus --record-type=A --record-data={} --record-name={}", ip_address, name);
-    println!("Creating DNS record:\n\t\t{}", record_str);
-    let create_record_result = command::run_host_cmd(&record_str);
-    if !create_record_result.success{
-        println!("Failed, with stderr:\n\n{}", create_record_result.stderr);
-    }
+        if ip_address == None {
+            error!("Couldn't locate droplet in output: {}", res_stdout);
+            return command::Result {
+                exit_code: None,
+                success: false,
+                stdout: "".to_string(),
+                stderr: res.stderr.clone()
+            }
+        }
+
+        command::Result {
+            exit_code: res.exit_code,
+            success: res.success,
+            // We check for None above
+            stdout: ip_address.unwrap().trim().to_string(),
+            stderr: res.stderr.clone()
+        }
+    };
+
+    let create_str = format!("doctl compute droplet create {} --image=ubuntu-16-04-x64 --region=sfo1 --size=512mb --ssh-keys=\"%ssh_keys%\" --wait", name);
+    let record_str = format!("doctl compute domain records create one.haus --record-type=A --record-data=%ip_address% --record-name={}", name);
+
+    let chain = chain::CommandChain::new()
+        .cmd("doctl compute ssh-key list --no-header --format=ID")
+        .result_mapped_cmd(&ssh_key_mapping_func, &create_str)
+        .cmd("doctl compute droplet list --format Name,PublicIPv4,PublicIPv6,Status")
+        .result_proc(&ip_address_extractor)
+        .result_mapped_cmd(&ip_address_mapping_func, &record_str)
+        .execute();
 }
 
 pub fn destroy_droplet_by_name(name: &str) {
