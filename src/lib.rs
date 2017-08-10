3edb1d3b78a1985926e8c053165182c9734bf1b3
Michael Xu <michaeljxu11@gmail.com>
2017-08-09 20:09:06 -0700

    added tests for command chain

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,63 @@ pub mod chain;
 
 #[cfg(test)]
 mod tests {
+
+    use super::chain;
+    use super::command;
+
     #[test]
-    fn it_works() {
+    fn basic_fatal_test_should_succeed() {
+        let res = chain::CommandChain::new()
+            .cmd("echo hello")
+            .execute();
+
+        assert!(res.result.unwrap().success);
+    }
+
+    #[test]
+    fn basic_fatal_test_should_fail() {
+        let res = chain::CommandChain::new()
+            .cmd("echo hello")
+            .cmd("test2")
+            .cmd("test3")
+            .execute();
+
+        assert!(!res.result.unwrap().success);
+    }
+
+    #[test]
+    fn basic_nonfatal_should_succeed() {
+        let res = chain::CommandChain::new()
+            .cmd_nonfatal("echo hello")
+            .cmd_nonfatal("restinpeace")
+            .cmd("echo yo")
+            .execute();
+
+        assert!(res.result.unwrap().success);
+    }
+
+
+    #[test]
+    fn basic_processing_func_test() {
+
+        let processing_func = |res: &command::Result| -> command::Result {
+            let mut extra_stdout = res.stdout.clone();
+            extra_stdout.push_str("+processing");
+
+            command::Result {
+                exit_code: res.exit_code,
+                success: res.success,
+                stdout: extra_stdout,
+                stderr: res.stderr.clone()
+            }
+        };
+
+        let res = chain::CommandChain::new()
+            .cmd("echo hello")
+            .result_proc(&processing_func)
+            .execute()
+            .execute();
+
+        assert!(res.result.unwrap().success);
     }
 }
