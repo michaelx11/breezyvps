40f04448435386c2ede38ed5f2326fc3be51da84
Michael Xu <michaeljxu11@gmail.com>
2017-08-09 20:49:46 -0700

    can't think of a good way to modify the vector based on other items

--- src/chain.rs
+++ src/chain.rs
@@ -6,7 +6,7 @@ pub enum Item<'a> {
     FatalCommand(String),
     NonFatalCommand(String),
     ResultProcessor(&'a Fn(&command::Result) -> command::Result),
-    CommandModifier(&'a Fn(&command::Result, Option<&'a mut Item>) -> Option<&'a mut Item<'a>>)
+    CommandModifier(&'a Fn(&command::Result, Item) -> Item<'a>)
 }
 
 pub struct CommandChain<'a> {
@@ -27,6 +27,11 @@ impl<'a> CommandChain<'a> {
 
     pub fn result_proc(mut self, f: &'a Fn(&command::Result) -> command::Result) -> Self {
         self.commands.push(Item::ResultProcessor(f));
+        self
+    }
+
+    pub fn command_modifier(mut self, f: &'a Fn(&command::Result, Item) -> Item<'a>) -> Self {
+        self.commands.push(Item::CommandModifier(f));
         self
     }
 
@@ -52,9 +57,11 @@ impl<'a> CommandChain<'a> {
 
     // Executes the chain and returns self, with vector reset
     pub fn execute(mut self) -> Self {
-        for item in self.commands.iter() {
+        for i in 0..commands.len() {
+            let owned_commands_clone = commands.clone();
+            let commands_len = owned_commands_clone.len();
             match item {
-                &Item::FatalCommand(ref s) => {
+                &mut Item::FatalCommand(ref s) => {
                     let result = CommandChain::run_command(s);
                     if !result.success {
                         self.result = Some(result);
@@ -62,11 +69,11 @@ impl<'a> CommandChain<'a> {
                     }
                     self.result = Some(result);
                 },
-                &Item::NonFatalCommand(ref s) => {
+                &mut Item::NonFatalCommand(ref s) => {
                     let result = CommandChain::run_command(s);
                     self.result = Some(result);
                 },
-                &Item::ResultProcessor(f) => {
+                &mut Item::ResultProcessor(f) => {
                     self.result = {
                         if let Some(ref mut curr_res) = self.result {
                             Some(f(&curr_res))
@@ -76,8 +83,19 @@ impl<'a> CommandChain<'a> {
                         }
                     };
                 },
-                &Item::CommandModifier(f) => {
-                    // TODO: implement me!
+                &mut Item::CommandModifier(f) => {
+                    if i >= commands_len - 1 {
+                        warn!("No next Item, leaving it unmodified");
+                    }
+                    let next_item = &mut commands[i + 1];
+                    commands[i + 1] = {
+                        if let Some(ref mut curr_res) = self.result {
+                            f(&curr_res, next_item.clone())
+                        } else {
+                            warn!("Current result is empty, no-op!");
+                            next_item.clone()
+                        }
+                    };
                 }
             }
         }
